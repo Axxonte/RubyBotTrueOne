f0d57882bcb1734d01dc896139d9d5d71fa05486
Axxonte <58403500+Axxonte@users.noreply.github.com>
2020-09-29 17:02:44 +0200

    Update UpdateCommand.java

--- src/main/java/fr/Axxonte/RubyBot/command/commands/Owner/UpdateCommand.java
+++ src/main/java/fr/Axxonte/RubyBot/command/commands/Owner/UpdateCommand.java
@@ -4,7 +4,9 @@ import fr.Axxonte.RubyBot.Config;
 import fr.Axxonte.RubyBot.command.CommandContext;
 import fr.Axxonte.RubyBot.command.ICommand;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class UpdateCommand implements ICommand {
     @Override
@@ -12,10 +14,19 @@ public class UpdateCommand implements ICommand {
         if (!ctx.getAuthor().getId().equals(Config.get("Owner_id")))
             return;
 
-        System.console().printf("pwd\n");
+        Process process = Runtime.getRuntime().exec("pwd");
+        printResults(process);
         ctx.getChannel().sendMessage("Now Updating from the GitHub :heart:").queue();
         //System.exit(0);
 
+    }
+
+    public static void printResults(Process process) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
     }
 
     @Override
